6779af2136512d0fa62252afc160f884cba76abe
Harsh Patel <hn6patel@uwaterloo.ca>
2023-06-20 18:55:48 -0400

    imported spotify sdk, doesnt work

--- app/src/main/java/com/example/musictoned/MainActivity.kt
+++ app/src/main/java/com/example/musictoned/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.musictoned
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.musictoned.ui.theme.MusicTonedTheme
+
+//import spotify from the src/libs/spotify-app-remote-release-0.7.2.aar
+
+
+private val clientId = "8b97731837b64c60b30ba2ecac7a8b74"
+private val redirectUri = "com.example.musictoned://callback"
+private var spotifyAppRemote: SpotifyAppRemote? = null
 
 /**
  * Influenced by composable UI example provided by Android
@@ -22,5 +30,34 @@ class MainActivity : ComponentActivity() {
                 MusicTonedNavHost()
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                // Now you can start interacting with App Remote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
+    }
+
+    private fun connected() {
+
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
