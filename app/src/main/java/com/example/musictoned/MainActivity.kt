c6b88a93000fa2af77de886b0cba8f91aa27915c
Harsh Patel <hn6patel@uwaterloo.ca>
2023-06-21 13:20:36 -0400

    added spotify android sdk

--- app/src/main/java/com/example/musictoned/MainActivity.kt
+++ app/src/main/java/com/example/musictoned/MainActivity.kt
@@ -5,15 +5,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.musictoned.ui.theme.MusicTonedTheme
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
 
 //import spotify from the src/libs/spotify-app-remote-release-0.7.2.aar
-
 
 private val clientId = "8b97731837b64c60b30ba2ecac7a8b74"
 private val redirectUri = "com.example.musictoned://callback"
 private var spotifyAppRemote: SpotifyAppRemote? = null
 
-/**
+/*
  * Influenced by composable UI example provided by Android
  * Ref: https://github.com/android/compose-samples/blob/main/Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/MainActivity.kt
  */
