133382089bb2f4779535748be90a570f8bc74ca4
Dalton Bown <daltonbown01@gmail.com>
2023-06-24 18:28:49 -0400

    Finished welcome screen

--- app/src/main/java/com/example/musictoned/Navigation.kt
+++ app/src/main/java/com/example/musictoned/Navigation.kt
@@ -5,11 +5,13 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.musictoned.Destinations.ABOUT_YOU_ROUTE
 import com.example.musictoned.Destinations.ROUTINES_ROUTE
 import com.example.musictoned.Destinations.ROUTINE_ROUTE
 import com.example.musictoned.Destinations.SPOTIFY_AUTHORIZATION_ROUTE
 import com.example.musictoned.Destinations.SPOTIFY_BETA_ROUTE
 import com.example.musictoned.Destinations.WELCOME_ROUTE
+import com.example.musictoned.aboutYou.AboutYouRoute
 import com.example.musictoned.routine.RoutineRoute
 import com.example.musictoned.routines.RoutinesRoute
 import com.example.musictoned.spotifyAuthorization.SpotifyAuthorizationRoute
@@ -26,6 +28,7 @@ import com.example.musictoned.welcome.WelcomeRoute
  */
 object Destinations {
     const val WELCOME_ROUTE = "welcome"
+    const val ABOUT_YOU_ROUTE = "aboutYou"
     const val SPOTIFY_AUTHORIZATION_ROUTE = "spotifyAuthorization"
     const val SPOTIFY_BETA_ROUTE = "spotifyBeta" // TODO - Remove, just for Harsh's testing
     const val ROUTINES_ROUTE = "routines"
@@ -40,7 +43,15 @@ fun MusicTonedNavHost(navController: NavHostController = rememberNavController()
     ) {
         composable(WELCOME_ROUTE) {
             WelcomeRoute(
-                onNavigateToSpotifyAuthorization = {
+                onClickCreateAnAccount = {
+                    navController.navigate(ABOUT_YOU_ROUTE)
+                }
+            )
+        }
+
+        composable(ABOUT_YOU_ROUTE) {
+            AboutYouRoute(
+                onClickContinue = {
                     navController.navigate(SPOTIFY_AUTHORIZATION_ROUTE)
                 }
             )
