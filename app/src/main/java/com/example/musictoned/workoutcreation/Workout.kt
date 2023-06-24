2e714032ddf9686bb10aaf518a67b5af37f0e20e
Shijia Chen <shijiajchen@gmail.com>
2023-06-24 19:10:36 -0400

    Change to data classes

--- app/src/main/java/com/example/musictoned/workoutcreation/Workout.kt
+++ app/src/main/java/com/example/musictoned/workoutcreation/Workout.kt
@@ -1,5 +1,7 @@
 package com.example.musictoned.workoutcreation
 
-data class Workout (private val id: Int, val exercises: List<WorkoutExercise> = listOf()){
-    override fun toString(): String = "Category [id: ${id}, exercises: ${this.exercises}]"
+data class Workout (private val id: Int, val exercises: ArrayList<WorkoutExercise> = arrayListOf<WorkoutExercise>() ){
+    fun addExercise(exercise: WorkoutExercise){
+        exercises.add(exercise)
+    }
 }
