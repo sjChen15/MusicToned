a7b5c047974ed3cc8239c1d695e74e5facfd84db
Shijia Chen <shijiajchen@gmail.com>
2023-06-25 13:34:49 -0400

    Changes for frontend usage

--- app/src/main/java/com/example/musictoned/workoutcreation/WorkoutCreationEnums.kt
+++ app/src/main/java/com/example/musictoned/workoutcreation/WorkoutCreationEnums.kt
@@ -3,3 +3,7 @@ package com.example.musictoned.workoutcreation
 enum class SongSetting {
     SHUFFLE, GLOBALSHUFFLE, SETSONG
 }
+
+enum class BpmMode {
+    AVERAGE, FAST, SLOW
+}
--- app/src/main/java/com/example/musictoned/workoutcreation/WorkoutExercise.kt
+++ app/src/main/java/com/example/musictoned/workoutcreation/WorkoutExercise.kt
@@ -1,6 +1,10 @@
 package com.example.musictoned.workoutcreation
 
 //input is workout name, time in seconds
-data class WorkoutExercise(private val workout: String, private var length: Int = 30, private var songSetting: SongSetting = SongSetting.SHUFFLE) {
-    private var song = "" //song name in string? depends on what info needed to play the song
-}
+data class WorkoutExercise(
+    private val workout: Exercise,
+    private var length: Int = 30,
+    private var songSetting: SongSetting = SongSetting.SHUFFLE,
+    private var bpmMode: BpmMode = BpmMode.AVERAGE,
+    private var song: String = "" //song name in string? depends on what info needed to play the song
+)
