54cb9565bc39daccb325ff6c57de8b889c66f7dd
Dalton Bown <daltonbown01@gmail.com>
2023-07-04 16:04:58 -0400

    Add new settings page

---
+++ app/src/main/java/com/example/musictoned/settings/SettingsScreen.kt
@@ -0,0 +1,110 @@
+package com.example.musictoned.settings
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.musictoned.R
+import com.example.musictoned.ui.theme.MusicTonedTheme
+import com.example.musictoned.util.supportWideScreen
+import com.example.musictoned.util.BottomBar
+import com.example.musictoned.util.BottomNavPages
+
+/**
+ * Influenced by composable UI example provided by Android
+ * Ref: https://github.com/android/compose-samples/blob/main/Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/signinsignup/WelcomeScreen.kt
+ */
+
+@Composable
+fun SettingsScreen(
+    onNavigateToRoutines: (charOffset: Int) -> Unit,
+    onNavigateToAnalytics: (charOffset: Int) -> Unit,
+    onNavigateToSettings: (charOffset: Int) -> Unit
+) {
+
+    Surface(
+        modifier = Modifier
+        .supportWideScreen()
+    ) {
+        Scaffold(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFFFFFFF))
+                .navigationBarsPadding(),
+            backgroundColor = Color.Transparent,
+            contentColor = Color.Transparent,
+            topBar = {
+                TopBar()
+            },
+            bottomBar = {
+                BottomBar(
+                    onNavigateToRoutines = onNavigateToRoutines,
+                    onNavigateToAnalytics = onNavigateToAnalytics,
+                    onNavigateToSettings = onNavigateToSettings,
+                    currentPage = BottomNavPages.SETTINGS
+                )
+            },
+            content = { innerPadding ->
+                Box ( modifier = Modifier.padding(innerPadding)){
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun TopBar(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+        ) {
+        Text(
+            text = "SETTINGS",
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp,
+            color = Color(94,96,206,255),
+            fontFamily = FontFamily(Font(R.font.roboto_regular)),
+            fontWeight = FontWeight(500),
+            letterSpacing = 0.46.sp,
+            modifier = Modifier
+                .padding(start = 24.dp)
+                .padding(top = 12.dp)
+        )
+        Divider(
+            color = Color(94,96,206,255),
+            thickness = 1.dp,
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, bottom = 4.dp)
+        )
+
+    }
+}
+
+@Preview(name = "Settings light theme", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+fun SettingsScreenPreview() {
+    MusicTonedTheme {
+        SettingsScreen(
+            onNavigateToRoutines = {},
+            onNavigateToAnalytics = {},
+            onNavigateToSettings = {}
+        )
+    }
+}
