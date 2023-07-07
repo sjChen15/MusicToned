9985576e09ed58d9a7986e5b009ea651c1dfc90c
Dalton Bown <daltonbown01@gmail.com>
2023-07-07 00:45:55 -0400

    Add imageId to the Exercise obj

--- app/src/main/java/com/example/musictoned/workoutcreation/Exercise.kt
+++ app/src/main/java/com/example/musictoned/workoutcreation/Exercise.kt
@@ -1,6 +1,10 @@
 package com.example.musictoned.workoutcreation
 
+import androidx.annotation.DrawableRes
+
 data class Exercise (
     var name: String,
     var target: List<String>,
-    var bpm: Int)
+    var bpm: Int,
+    @DrawableRes var imageId: Int
+)
