b809c180995a2a8e7a4435fd9b58dc913704e5bc
Dalton Bown <daltonbown01@gmail.com>
2023-07-08 00:12:16 -0400

    Fix bug where legacy workouts could be stored with null vals causing the .hashCode func to crash

--- app/src/main/java/com/example/musictoned/workoutcreation/Exercise.kt
+++ app/src/main/java/com/example/musictoned/workoutcreation/Exercise.kt
@@ -1,10 +1,8 @@
 package com.example.musictoned.workoutcreation
 
-import androidx.annotation.DrawableRes
-
 data class Exercise (
-    var name: String,
-    var target: List<String>,
-    var bpm: Int,
-    @DrawableRes var imageId: Int
+    var name: String = "",
+    var target: List<String> = listOf(),
+    var bpm: Int = 0,
+    var imageName: String = ""
 )
