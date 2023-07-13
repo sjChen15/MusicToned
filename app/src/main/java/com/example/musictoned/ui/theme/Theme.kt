283c25f595567648509b0ccc8c20ddd86fa589a5
Daniel <daniel.adamsmac@gmail.com>
2023-07-13 19:37:17 -0400

    added BottomWave composable to Theme.kt so that consistent theme can more easily be maintained across different screens.

--- app/src/main/java/com/example/musictoned/ui/theme/Theme.kt
+++ app/src/main/java/com/example/musictoned/ui/theme/Theme.kt
@@ -2,7 +2,11 @@ package com.example.musictoned.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,10 +14,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import com.example.musictoned.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -66,5 +84,51 @@ fun MusicTonedTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
+    )
+}
+
+@Composable
+fun BottomWaves(
+    withBottomBar: Boolean = true
+){
+
+    val configuration = LocalConfiguration.current
+
+    val screenHeight = configuration.screenHeightDp.dp
+    //Log.d("SCREEN HEIGHT: ", screenHeight.toString())
+
+    var componentHeight by remember { mutableStateOf(0.dp) }
+    //Log.d("IMAGE HEIGHT", componentHeight.toString())
+
+    // get local density from composable
+    val density = LocalDensity.current
+
+    var yOffset = 0.dp;
+
+    if(withBottomBar){
+        yOffset = 567.dp
+    } else {
+        yOffset = 567.dp + BOTTOM_BAR_HEIGHT.dp
+    }
+
+    Image(
+        painter = painterResource(id = R.drawable.routines_waves),
+        contentDescription = "Routines Waves",
+        alignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .fillMaxWidth()
+            .onGloballyPositioned {
+                componentHeight = with(density) {
+                    it.size.height.toDp()
+                }
+            }
+            //I think the top and bottom bars of the phone that are not part of our app
+            //are affecting the calculation of the screen height (The blue bar at the top, and the
+            //black bar at the bottom)
+            //either that, or it's offsetting from the lazy grid
+            //.offset(x = 0.dp, y = screenHeight - ( BOTTOM_BAR_HEIGHT.dp + componentHeight))
+            .offset(x = 0.dp, y = 567.dp)
+            .background(color = Color.Transparent),
+        contentScale = ContentScale.FillWidth,
     )
 }
--- app/src/main/java/com/example/musictoned/ui/theme/screenSizingConstants.kt
+++ app/src/main/java/com/example/musictoned/ui/theme/screenSizingConstants.kt
@@ -1,2 +1,6 @@
 package com.example.musictoned.ui.theme
 
+import androidx.compose.ui.unit.dp
+
+const val BOTTOM_BAR_HEIGHT = 50
+
