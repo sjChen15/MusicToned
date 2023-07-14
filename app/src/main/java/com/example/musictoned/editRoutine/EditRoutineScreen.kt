9e46431891bf6baac2b6b68da2a814daf7f960a2
Winston <w5so@uwaterloo.ca>
2023-07-14 18:01:01 -0400

    got swap working cant get duration and tempo to swap

--- app/src/main/java/com/example/musictoned/editRoutine/EditRoutineScreen.kt
+++ app/src/main/java/com/example/musictoned/editRoutine/EditRoutineScreen.kt
@@ -1,6 +1,7 @@
 package com.example.musictoned.editRoutine
 
 import android.content.res.Configuration
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -21,6 +22,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -39,6 +41,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.*
 import androidx.compose.material.TextFieldDefaults
@@ -46,6 +50,7 @@ import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -94,9 +99,13 @@ fun EditRoutineScreen(
     onNavigateToAddExercise: () -> Unit,
 ) {
 
-    val workout = AllWorkouts.getWorkoutInProgress()
+    var workout by remember { mutableStateOf(AllWorkouts.getWorkoutInProgress()) }
 
     var popupControl by remember { mutableStateOf(false) }
+
+    var exercises = remember { mutableStateListOf<WorkoutExercise>() }
+
+    exercises.addAll(workout.exercises.toList())
 
     Surface(modifier = Modifier
         .supportWideScreen()
@@ -117,8 +126,10 @@ fun EditRoutineScreen(
                             val exerciseName = it
                             //add exercise to current workout
                             if (exerciseName != null.toString()){
-                                workout.addExercise(WorkoutExercise(ExerciseTempos.getExercise(exerciseName.toString())))
+                                workout.addExercise(WorkoutExercise(ExerciseTempos.getExercise(exerciseName)))
                                 workout.deleteLastExercise()
+                                exercises.clear()
+                                exercises.addAll(workout.exercises)
                             }
                         }
                     )
@@ -148,11 +159,32 @@ fun EditRoutineScreen(
             },
             content = { innerPadding ->
                 Box ( modifier = Modifier.padding(innerPadding)){
-                    Exercises(
-                        modifier = Modifier.padding(start = 25.dp, end = 25.dp),
-                        workout = workout,
-                        contentPadding = innerPadding,
-                    )
+                    key(exercises){
+                        LazyColumn {
+                            items(exercises) { exercise ->
+                                var duration by remember { mutableStateOf(exercise.getLength()) }
+                                Exercise( exercise = exercise,
+                                    onMoveUp = {
+                                        val index = workout.exercises.indexOf( it )
+                                        if ( index != 0 ){
+                                            workout.reorderExercise( it, index - 1 )
+                                        }
+                                        exercises.clear()
+                                        exercises.addAll(workout.exercises)
+                                    },
+                                    onMoveDown = {
+                                        val index = workout.exercises.indexOf( it )
+                                        if ( index != workout.exercises.size - 1 ){
+                                            workout.reorderExercise( it, index + 1 )
+                                        }
+                                        duration = exercises[exercises.indexOf(exercise)].getLength()
+                                        exercises.clear()
+                                        exercises.addAll(workout.exercises)
+                                    }
+                                )
+                            }
+                        }
+                    }
                 }
             }
         )
@@ -227,29 +259,15 @@ private fun TopBar(
     }
 }
 
-@Composable
-private fun Exercises(
-    modifier: Modifier,
-    workout: Workout,
-    contentPadding: PaddingValues,
-){
-    Column(
-        modifier = modifier
-            .verticalScroll(rememberScrollState())
-    ){
-        workout.exercises.forEach {exercise ->
-            Exercise( exercise = exercise)
-        }
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun Exercise(
     modifier: Modifier = Modifier,
     exercise: WorkoutExercise,
+    onMoveUp: ( exercise: WorkoutExercise ) -> Unit,
+    onMoveDown: ( exercise: WorkoutExercise ) -> Unit,
 ){
-    var text by remember { mutableStateOf(exercise.getLength().toString()) }
+    var duration by remember { mutableStateOf(exercise.getLength().toString()) }
     val interactionSource = remember { MutableInteractionSource() }
 
     Column( modifier = modifier.padding(bottom=15.dp)){
@@ -274,14 +292,36 @@ fun Exercise(
                     .padding(top = 15.dp, bottom = 15.dp),
                 verticalAlignment = Alignment.CenterVertically,
             ){
-                Image(
-                    painter = painterResource(id = R.drawable.drag_drop_icon),
-                    modifier = modifier
-                        .padding(end = 20.dp, start = 20.dp)
-                        .height(20.dp),
-                    contentDescription = "Shuffle button",
-                    contentScale = ContentScale.FillHeight,
-                )
+                Column(){
+                    Button(
+                        onClick = {
+                            onMoveUp( exercise )
+                        }
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.drag_drop_icon),
+                            modifier = modifier
+                                .padding(end = 20.dp, start = 20.dp)
+                                .height(20.dp),
+                            contentDescription = "Move up",
+                            contentScale = ContentScale.FillHeight,
+                        )
+                    }
+                    Button(
+                        onClick = {
+                            onMoveDown( exercise )
+                        }
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.drag_drop_icon),
+                            modifier = modifier
+                                .padding(end = 20.dp, start = 20.dp)
+                                .height(20.dp),
+                            contentDescription = "Move down",
+                            contentScale = ContentScale.FillHeight,
+                        )
+                    }
+                }
                 Column(
                     modifier = modifier
                         .wrapContentHeight(align = Alignment.CenterVertically)
@@ -304,8 +344,9 @@ fun Exercise(
                             fontSize = 15.sp,
                         )
                         BasicTextField(
-                            value = text,
-                            onValueChange = { text = it },
+                            value = duration,
+                            onValueChange = {
+                                duration = it },
                             modifier = Modifier
                                 .height(30.dp)
                                 .padding(start = 10.dp)
@@ -315,7 +356,7 @@ fun Exercise(
                             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                         ) { innerTextField ->
                             TextFieldDefaults.OutlinedTextFieldDecorationBox(
-                                value = text,
+                                value = duration,
                                 innerTextField = innerTextField,
                                 enabled = true,
                                 singleLine = true,
