583ac8ad7728cefbdc0466751f57a195ce12aa4a
Daniel <daniel.adamsmac@gmail.com>
2023-07-15 13:07:42 -0400

    re added imports

--- app/src/main/java/com/example/musictoned/editRoutine/EditRoutineScreen.kt
+++ app/src/main/java/com/example/musictoned/editRoutine/EditRoutineScreen.kt
@@ -41,7 +41,9 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
